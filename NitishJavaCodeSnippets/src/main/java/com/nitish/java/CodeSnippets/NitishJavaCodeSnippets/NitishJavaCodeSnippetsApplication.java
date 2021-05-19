5d5ac39a04c78610d983ab9fff13b51a75d248ec
nitishgondkar <nitish.gondkar@gmail.com>
2021-05-19 21:39:47 +0530

    JSON Reader

---
+++ NitishJavaCodeSnippets/src/main/java/com/nitish/java/CodeSnippets/NitishJavaCodeSnippets/NitishJavaCodeSnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.java.CodeSnippets.NitishJavaCodeSnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitishJavaCodeSnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitishJavaCodeSnippetsApplication.class, args);
+	}
+
+}
---
+++ NitishJavaCodeSnippets/src/test/java/com/nitish/java/CodeSnippets/NitishJavaCodeSnippets/NitishJavaCodeSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.java.CodeSnippets.NitishJavaCodeSnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitishJavaCodeSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
