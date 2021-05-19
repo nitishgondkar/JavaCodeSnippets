ce79355f48c62d0219ce232346809a28ae76f44d
nitishgondkar <nitish.gondkar@gmail.com>
2021-05-19 21:54:56 +0530

    JSON Reader Code

---
+++ JsonReader/src/main/java/com/nitish/JsonReader/JsonReader/JsonReaderApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.JsonReader.JsonReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonReaderApplication.class, args);
+	}
+
+}
